f28fdd0f76ddbe8f8141f9009c4d3f400b1d108f
Dominik Kus <dominik@kdtk.de>
2019-08-06 14:05:02 +0200

    Enabled creation of link definition in the link-generator

--- src/link-generator.ts
+++ src/link-generator.ts
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 import log from 'loglevel';
 import { OpenAPIV3 } from 'openapi-types';
-import { isNumber } from 'util';
 import { resolveComponentRef } from './openapi-tools';
 
 interface PotentialLink {
@@ -24,7 +23,7 @@ function findPotentialLinkPairs(oas: OpenAPIV3.Document): PotentialLink[] {
       pO != null &&
       pO.responses != null &&
       Object.keys(pO.responses)
-        .map(parseInt)
+        .map(key => parseInt(key, 10))
         .some(code => !isNaN(code) && code >= 200 && code < 300)
     );
   });
@@ -67,8 +66,15 @@ function processLinkParameters(oas: OpenAPIV3.Document, links: PotentialLink[]):
     const fromGet = oas.paths[link.from].get as OpenAPIV3.OperationObject;
     const toGet = oas.paths[link.to].get as OpenAPIV3.OperationObject;
 
+    // TODO incorporate parameters under the path item
     const fromParams = fromGet.parameters != null ? dereferenceParameters(oas, fromGet.parameters) : [];
     const toParams = toGet.parameters != null ? dereferenceParameters(oas, toGet.parameters) : [];
+
+    // We can not handle cookie parameters as of now
+    if (toParams.some(parameter => parameter.in === 'cookie')) {
+      log.warn(`Cookie parameters are currently not supported. Ignoring path '${link.to}'.`);
+      continue;
+    }
 
     // We use a simple heuristic: We assume that parameters with the same name and same schema have identical meaning
     // across different operations. Therefore, we filter the potential links where all parameters for the to-operation
@@ -90,7 +96,7 @@ function processLinkParameters(oas: OpenAPIV3.Document, links: PotentialLink[]):
         ...link,
         parameterMap
       });
-      log.debug(`  Valid link found: '${link.from}' => '${link.to}'`);
+      log.debug(`  Valid link found: '${link.from}' => '${link.to}', ${parameterMap.size} parameter(s)`);
     }
   }
 
@@ -100,8 +106,61 @@ function processLinkParameters(oas: OpenAPIV3.Document, links: PotentialLink[]):
 
 export default function addLinkDefinitions(oas: OpenAPIV3.Document) {
   const potLinks = processLinkParameters(oas, findPotentialLinkPairs(oas));
+  potLinks.forEach(potLink => {
+    const fromGet = oas.paths[potLink.from].get as OpenAPIV3.OperationObject;
+    const fromResponses = fromGet.responses as OpenAPIV3.ResponsesObject;
+
+    // All responses objects for successful response codes for a get request.
+    // $refs are resolved but deduplicated with _.uniq.
+    // We know that this array is non empty because we filtered those in 'findPotentialLinkPairs'.
+    const successGetResponses = _.uniq(
+      Object.keys(fromResponses)
+        .map(key => parseInt(key, 10))
+        .filter(code => !isNaN(code) && code >= 200 && code < 300)
+        .map(code => {
+          const obj = fromResponses[code];
+          if ('$ref' in obj) {
+            return resolveComponentRef(oas, obj, 'responses');
+          } else {
+            return obj;
+          }
+        })
+    );
+
+    // We now add a new link definition to the components section and reference it in every response
+    if (oas.components == null) {
+      oas.components = {};
+    }
+    if (oas.components.links == null) {
+      oas.components.links = {};
+    }
+    let linkName = _.last(potLink.to.split('/')) as string;
+    while (linkName in oas.components.links) {
+      linkName += '1';
+    }
+    const parametersObject: { [parameter: string]: any } = {};
+    potLink.parameterMap.forEach((toParam, fromParam) => {
+      // fromParam.in can only be 'query', 'header', 'path', 'cookie' according to the definition.
+      // We have ruled out 'cookie' in 'processLinkParameters', so this is a valid Runtime Expression.
+      parametersObject[toParam.name] = `$request.${fromParam.in}.${fromParam.name}`;
+    });
+    oas.components.links[linkName] = {
+      description: `Automatically generated link definition`,
+      operationRef: `#/paths/${potLink.from.replace(/\//g, '~1')}/get`,
+      parameters: parametersObject
+    };
+
+    successGetResponses.forEach(response => {
+      if (response.links == null) {
+        response.links = {};
+      }
+      response.links[linkName] = {
+        $ref: `#/components/links/${linkName}`
+      };
+    });
+  });
   // TODO actually add the link definitions to the oas
-  console.log(potLinks);
+  // console.log(potLinks);
 
   return oas;
 }
